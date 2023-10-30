23ea3df4c2a42ce8ba0ec05e865d3e0b9e601919
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2023-10-30 14:27:33 -0400

    small button addition

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,7 +2,7 @@ import './button.css'
 
 export default function Button(props) {
     return (
-        <a className={'button ' + props.color} href={props.href}>
+        <a className={'button ' + props.color} href={props.href} title={props.label}>
             {props.label || props.children}
         </a>
     )
