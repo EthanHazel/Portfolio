bc12b3275e0bac905cec2da627b47778b40111a7
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2023-10-31 16:26:09 -0400

    ported over features from the OG portfolio

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,11 +1,42 @@
-import './container.css';
+import React, { useEffect, useRef, useState } from 'react';
+import './Container.css';
 
 export default function Container(props) {
   const containerClass = `${props.size}Container`;
 
+  const [isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef(null);
+
+  // Intersection Observer callback function
+  const handleIntersect = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      setIsVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5, // Change this threshold as needed
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className={containerClass}>
-      <div className="container">
+    <div className={containerClass} ref={containerRef}>
+      <div className={`container ${isVisible ? 'fade-in' : ''}`}>
         {props.children}
       </div>
     </div>
