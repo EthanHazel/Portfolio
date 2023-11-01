e0a5c05927778390f28da9ff0936e75d08942655
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2023-11-01 00:14:01 -0400

    added dynamic drop shadow to face image

--- src/components/face.jsx
+++ src/components/face.jsx
@@ -1,12 +1,52 @@
-import './face.css'
+import React, { useRef, useState, useEffect } from 'react';
+import './face.css';
 
 export default function Face(props) {
+    const faceImageRef = useRef(null);
+    const [shadowX, setShadowX] = useState(0);
+    const [shadowY, setShadowY] = useState(0);
+
+    useEffect(() => {
+        const faceImage = faceImageRef.current;
+
+        const updateShadow = (e) => {
+            const boundingBox = faceImage.getBoundingClientRect();
+            const centerX = boundingBox.left + boundingBox.width / 2;
+            const centerY = boundingBox.top + boundingBox.height / 2;
+            const angle = Math.atan2(e.clientY - centerY, e.clientX - centerX);
+            const distance = Math.min(Math.hypot(e.clientX - centerX, e.clientY - centerY), 100);
+
+            const offsetX = distance * Math.cos(angle);
+            const offsetY = distance * Math.sin(angle);
+
+            setShadowX(offsetX / 50); // Adjust the values as needed
+            setShadowY(offsetY / 50); // Adjust the values as needed
+        };
+
+        const onMouseMove = (e) => {
+            updateShadow(e);
+        };
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        return () => {
+            document.removeEventListener('mousemove', onMouseMove);
+        };
+    }, []);
+
     return (
         <div className='faceContainer'>
             <img className='faceOverlay' src='../assets/photo_gradient_overlay.png' />
-            <img className='faceImage' src={props.source} />
+            <img
+                className='faceImage'
+                ref={faceImageRef}
+                src={props.source}
+                style={{
+                    filter: `drop-shadow(${-shadowX}vh ${-shadowY}vh 3vh var(--background))`,
+                }}
+            />
             <img className='faceBackground' src='../assets/photo_gradient.png' />
             <img className='faceBackgroundBlur' src='../assets/photo_gradient.png' />
         </div>
-    )
+    );
 }
